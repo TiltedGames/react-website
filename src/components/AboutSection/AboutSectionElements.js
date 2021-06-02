6b78dce8721c3c4c312c82698a95a0bd6e2f5b22
muerto1560 <noahcorona@gmail.com>
2021-06-02 11:24:38 -0700

    add dynamic columns to about page, add new font, change monospace font to poppins

--- src/components/AboutSection/AboutSectionElements.js
+++ src/components/AboutSection/AboutSectionElements.js
@@ -2,91 +2,65 @@ import styled from 'styled-components'
 import aboutBg from '../../img/stock_1.jpg'
 
 export const AboutContainer = styled.div `
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  margin-left: 75px;
+  max-width: calc(85vw - 75px);
   padding: 0;
   position: relative;
   z-index: 1;
+  
+
+  :after {
+    content: "";
+    display: table;
+    clear: both;
+  }
 `
 
-export const AboutBackground = styled.div `
-  width: inherit;
-  height: inherit;
-  overflow: hidden;
+export const Column_Dynamic = styled.div `
+  float: left;
+  width: 40vw;
 `
 
-export const VideoBackground = styled.video `
-  z-index: -1000;
-  position: absolute;
-  width: 100vw;
-  margin-left: -40px;
-  height: inherit;
-  overflow: hidden;
-  background: #282828;
+export const Column_FullWidth = styled.div `
+
 `
 
-export const AboutContent = styled.div `
-  z-index: 3;
-  max-width: 70vw;
-  position: absolute;
-  top: 40px;
-  left: 40px;
-  display: flex;
-  flex-direction: column;
-`
-
-export const AboutHeader = styled.span `
+export const AboutHeader = styled.div `
+  font-family: 'NeutralFace';
   color: #fff;
-  font-size: 200px;
-  text-align: left;
+  font-size: 160px;
+  text-align: right;
   padding: 0;
-  margin-bottom: 60px;
+  margin: 0;
+  font-weight: bold;
   
   @media screen and (max-width: 2048px) {
-    font-size: 180px;
+    font-size: 120px;
   }
 
   @media screen and (max-width: 1080px) {
-    font-size: 160px;
-    max-width: 60vw;
+    font-size: 90px;
   }
   
   @media screen and (max-width: 768px) {
     font-size: 60px;
-    max-width: 40vw;
   }
 
   @media screen and (max-width: 480px) {
     font-size: 50px;
-    max-width: 40vw;
   }
 `
 
 export const AboutInfo = styled.div `
-  max-width: 70vw;
-  padding:30px;
-  height: min-content;
-  background: url(${ aboutBg }) no-repeat;
-
-  @media screen and (max-width: 1080px) {
-    max-width: 60vw;
-  }
-
-  @media screen and (max-width: 768px) {
-    max-width: 40vw;
-  }
-
-  @media screen and (max-width: 480px) {
-    max-width: 40vw;
-  }
+  padding: 30px 0;
 `
 
 export const AboutSubheader = styled.h1 `
-  color: #000;
   font-size: 100px;
   text-align: left;
-  padding: 60px 0;
+  margin: 0;
+  background: #fff;
+  color: #000;
   
   @media screen and (max-width: 2048px) {
     font-size: 90px;
@@ -106,11 +80,13 @@ export const AboutSubheader = styled.h1 `
 `
 
 export const AboutP = styled.p `
-  margin-top: 250px;
-  color: #000;
   font-size: 48px;
   text-align: left;
-  padding: 0;
+  margin: 0;
+  font-weight: bold;
+  background: #000;
+  color: #fff;
+  width: 100%;
   
   @media screen and (max-width: 768px) {
     font-size: 36px;
--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -3,13 +3,11 @@ import Video from '../../video/video.mp4'   // import background video
 // import game section elements
 import {
     AboutContainer,
-    AboutBackground,
-    VideoBackground,
     AboutHeader,
     AboutContent,
     AboutP,
     AboutSubheader,
-    AboutInfo
+    AboutInfo, Column_Dynamic
 } from "./AboutSectionElements";
 
 const AboutSection = () => {
@@ -21,19 +19,21 @@ const AboutSection = () => {
 
     return (
         <AboutContainer>
-            <AboutContent>
+            <Column_Dynamic>
                 <AboutHeader>
                     We are Tilted Games
                 </AboutHeader>
-                <AboutInfo>
+            </Column_Dynamic>
+            <Column_Dynamic>
                     <AboutSubheader>
                         A small team of passionate game developers, artists, and engineers
                     </AboutSubheader>
+            </Column_Dynamic>
+            <Column_Dynamic>
                     <AboutP>
                         And we're looking to bring a new and exciting experience to you! We're working on a game named Recoup. At the moment, all of our software is open source, covered by the MIT license.
                     </AboutP>
-                </AboutInfo>
-            </AboutContent>
+            </Column_Dynamic>
         </AboutContainer>
     );
 }
