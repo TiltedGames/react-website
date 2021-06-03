90dea4c038515fa6a5e7e7e9eb424af245bd67c0
muerto1560 <noahcorona@gmail.com>
2021-06-03 11:46:55 -0700

    add feature descriptions to one click server

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -45,6 +45,30 @@ const ToolsSection = () => {
             </Row>
             <Row>
                 <Column_FullWidth>
+                    <BoldText>
+                        Lightning Fast<br />
+                        Speed, where you need it most!
+                    </BoldText>
+                    <BoldText>
+                        Voice Chat<br />
+                        Talk with your friends on an encrypted voice server
+                    </BoldText>
+                    <BoldText>
+                        Text Chat<br />
+                        No mic? No problem! Chat with your friends via text
+                    </BoldText>
+                    <BoldText>
+                        Built for ML<br />
+                        Hand-crafted from the ground up to support machine learning
+                    </BoldText>
+                    <BoldText>
+                        .NET Web Sockets<br />
+                        Built on a strong foundation, developed and supported by Microsoft
+                    </BoldText>
+                    <BoldText>
+                        Storage<br />
+                        Save and load network data incredibly fast using Redis!
+                    </BoldText>
                     <DownloadButtonWrapper
                         onMouseEnter={ onHover }
                         onMouseLeave={ onHover }
